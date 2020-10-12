636505e4788e90c98a4242d9d39b6a5f5eb0c516
AlexeyMit2000 <72440729+AlexeyMit2000@users.noreply.github.com>
2020-10-12 10:53:20 +0300

    Add files via upload

---
+++ Cycles/Task4.java
@@ -0,0 +1,20 @@
+class Task4
+{
+    public static void main(String[]args)    
+    {
+        int n=12,buf=0;
+        double  e=2.71,M=0;
+        for(int x=1;x<=n;x++)
+        {
+            System.out.println("a["+x+"]= "+(1/(Math.pow((x-1),2))));
+            M=Math.abs(1/(Math.pow((x-1),2)));
+            if(M<e)
+            {
+                e=M;
+                buf=x;
+            }
+        }
+        System.out.println(M+" - самое маленькое значение с номером "+buf);
+    }
+}
+
